905b41a82a9cf820eab8e5ba1395c764297f4e48
Bezbo <without.a.beard@gmail.com>
2013-09-21 21:42:05 +0400

    first commit

---
+++ app/models/item.rb
@@ -0,0 +1,3 @@
+class Item < ActiveRecord::Base
+  attr_accessible :discription, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fm::Application.config.secret_token = 'b996cbb322c46196beecd14a68169624391f69ac48ed7f8d47b14b7b0b55694311dfa6dfb194863c3b0867a1567ce77978b51ae9909428b80cbe425d77115b5c'
