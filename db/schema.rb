751e9b26537017d69f01fa10f0aed7dd5f7e794f
Bezbo <without.a.beard@gmail.com>
2013-11-23 20:41:21 +0400

    add migration 20131116114721_create_users.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131107231710) do
+ActiveRecord::Schema.define(:version => 20131116114721) do
 
   create_table "items", :force => true do |t|
     t.string   "name"
